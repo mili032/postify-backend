5dc66cfb7c2067e188c3e525d625610104fb8b9c
Stefan Milijanović <stefan.milijanovic@protokol.io>
2025-05-21 22:47:24 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`api/v1`);
-
+  app.enableCors({
+    origin: ['*','http://localhost:3001'],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
